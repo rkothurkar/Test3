4eb9561dd5ebbcd5ac87364b21124b22dbf6c656
rkothurkar <r.kothurkar@somaiya.edu>
2025-12-29 13:59:14 +0530

    Update pjt2.py

--- pjt2.py
+++ pjt2.py
@@ -7,7 +7,7 @@ model = joblib.load("model.pkl")
 
 # App title
 st.title("Simple Linear Regression App")
-st.title("Simple Linear Regression App2")
+
 # User input
 x_value = st.number_input("Enter X value", value=1)
 
@@ -16,4 +16,5 @@ y_pred = model.predict(np.array([[x_value]]))
 
 # Show output
 st.write("Predicted Y value:", y_pred[0])
+
 
